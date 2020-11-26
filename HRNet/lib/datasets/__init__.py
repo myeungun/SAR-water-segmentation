8e6e87e6f3ea82be81a406bcc91aeeaf97cb86a7
Myeungun Kim <myoungunkim92@gmail.com>
2020-11-26 15:09:41 +0900

    Add files via upload

---
+++ HRNet/lib/datasets/__init__.py
@@ -0,0 +1,12 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Ke Sun (sunk@mail.ustc.edu.cn)
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .K5 import K5 as K5
+from .K5_DEM import K5_DEM as K5_DEM
